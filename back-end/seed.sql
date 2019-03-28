c6d863550132b97e4682dd14ce81a6f21a46da81
Ronihe <herongrong2011@gmail.com>
2019-03-28 15:26:53 -0700

    added batabase

--- back-end/seed.sql
+++ back-end/seed.sql
@@ -1,11 +1,11 @@
 DROP DATABASE if exists inputs_db;
 CREATE DATABASE inputs_db;
 
-\c jokes_db
+\c inputs_db
 
 CREATE TABLE jokes
 (
   id TEXT PRIMARY KEY,
   input TEXT NOT NULL,
-  votes INTEGER DEFAULT 0
+  -- votes INTEGER DEFAULT 0
 );
