a8a45f3ddf7d6edac9f2cd8e746f0c91ba4a0999
Ronihe <herongrong2011@gmail.com>
2019-03-28 17:05:47 -0700

    the data

--- back-end/Input.js
+++ back-end/Input.js
@@ -1,6 +1,21 @@
 const db = require('./db');
 
+/** Related functions for Inputs. */
 class Input {
-  // create an input
-  // get all the inputs
+  /** Get all inputs */
+  static async getAll() {
+    const inputsRes = await db.query('SELECT * FROM inputs;');
+    return inputsRes.rows;
+  }
+
+  static async createOne(data) {
+    // error handling, if there is no input, should be handled inthe front end
+    const result = await db.query(
+      `INSERT INTO inputs (input) VALUES ($1) RETURNING *`,
+      [data.input]
+    );
+
+    return result.rows[0];
+  }
 }
+module.exports = Input;
