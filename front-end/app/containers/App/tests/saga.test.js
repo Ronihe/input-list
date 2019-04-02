908a260d62055b53491bc4bebbc9e23a502d800d
Ronihe <herongrong2011@gmail.com>
2019-04-02 10:25:54 -0700

    did saga testing

--- front-end/app/containers/App/tests/saga.test.js
+++ front-end/app/containers/App/tests/saga.test.js
@@ -16,10 +16,10 @@ import {
 } from '../actions';
 
 import defaultSaga, {
-  fetchMessages,
-  sendMessage,
-  loadMessages,
-  sendNewMessage,
+  fetchInputs,
+  loadInputs,
+  postNewInput,
+  sendNewInput,
 } from '../saga';
 
 const watcherSagas = defaultSaga();
@@ -27,53 +27,53 @@ const watcherSagas = defaultSaga();
 describe('all sagas watcher', () => {
   it('should watch for other actions', () => {
     const effect = watcherSagas.next().value;
-    expect(effect).toEqual(all([loadMessages(), sendNewMessage()]));
+    expect(effect).toEqual(all([loadInputs(), sendNewInput()]));
   });
 });
 
-describe('fetchMessages saga', () => {
-  let loadMessagesGenerator;
+describe('fetchInputs saga', () => {
+  let loadInputsGenerator;
 
   // have to test twice, once for a successful load and once for an error
   // the beforeEach calls all the yields that has to happen before each of the test.
   beforeEach(() => {
-    loadMessagesGenerator = fetchMessages();
-    const callDescriptor = loadMessagesGenerator.next().value;
+    loadInputsGenerator = fetchInputs();
+    const callDescriptor = loadInputsGenerator.next().value;
     expect(callDescriptor).toMatchSnapshot();
   });
-  it('should dispatch the loadMessagesSuccess action if it calls API successfully', () => {
+  it('should dispatch the loadInputsSuccess action if it calls API successfully', () => {
     const response = [{ id: 1, message: 'hello' }, { id: 2, message: 'world' }];
-    const putDescriptor = loadMessagesGenerator.next(response).value;
-    expect(putDescriptor).toEqual(put(loadMessagesSuccess(response)));
+    const putDescriptor = loadInputsGenerator.next(response).value;
+    expect(putDescriptor).toEqual(put(loadInputsSuccess(response)));
   });
   it('should dispatch the loadMessagesError if the API call errors', () => {
     const response = new Error('some error');
-    const putDescriptor = loadMessagesGenerator.throw(response).value;
-    expect(putDescriptor).toEqual(put(loadMessagesError(response)));
+    const putDescriptor = loadInputsGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(loadInputsError(response)));
   });
 });
 
-describe('sendMessage saga', () => {
-  let sendMessageGenerator;
+describe('sendInputs saga', () => {
+  let sendNewInputGenerator;
 
   // have to test twice, once for a successful load and once for an error
   // the beforeEach calls all the yields that has to happen before each of the test.
   beforeEach(() => {
-    sendMessageGenerator = sendMessage();
-    const selectDescriptor = sendMessageGenerator.next().value;
+    sendNewInputGenerator = postNewInput();
+    const selectDescriptor = sendNewInputGenerator.next().value;
     expect(selectDescriptor).toMatchSnapshot();
 
-    const callDescriptor = sendMessageGenerator.next('message').value;
+    const callDescriptor = sendNewInputGenerator.next('message').value;
     expect(callDescriptor).toMatchSnapshot();
   });
   it('should dispatch the sendNewMessageSuccess action if it posts to the API successfully', () => {
     const newMessage = { id: 1, message: 'hello' };
-    const putDescriptor = sendMessageGenerator.next(newMessage).value;
-    expect(putDescriptor).toEqual(put(sendNewMessageSuccess(newMessage)));
+    const putDescriptor = sendNewInputGenerator.next(newMessage).value;
+    expect(putDescriptor).toEqual(put(sendNewInputSuccess(newMessage)));
   });
   it('should dispatch the sendNewMessageError action if the API errors', () => {
     const response = new Error('oh no');
-    const putDescriptor = sendMessageGenerator.throw(response).value;
-    expect(putDescriptor).toEqual(put(sendNewMessageError(response)));
+    const putDescriptor = sendNewInputGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(sendNewInputError(response)));
   });
 });
